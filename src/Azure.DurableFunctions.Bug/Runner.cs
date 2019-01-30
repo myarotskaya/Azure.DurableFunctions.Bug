85c2c6149585672690b8d39c6544f24e32c3acc1
Marta Yarotskaya <mvyarotskaya@gmail.com>
2019-01-30 17:46:45 +0800

    Initial commit

---
+++ src/Azure.DurableFunctions.Bug/Runner.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
+
+namespace Azure.DurableFunctions.Bug
+{
+    public class Runner
+    {
+        [FunctionName("TriggerViaHttp")]
+        public static Task<string> TriggerViaHttpAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "start")] HttpRequest request,
+            [OrchestrationClient] DurableOrchestrationClient orchestrationClient,
+            ExecutionContext executionContext,
+            CancellationToken cancellationToken) =>
+            orchestrationClient.StartNewAsync("StartOrchestration", new { });
+
+        [FunctionName("StartOrchestration")]
+        public static Task StartOrchestrationAsync(
+            [OrchestrationTrigger] DurableOrchestrationContext context,
+            ExecutionContext executionContext,
+            CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
